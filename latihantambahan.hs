0e66bfcd16e021e472c408d841c5d55924f25eec
Satryaji Aulia <satraul@macports.org>
2019-10-18 23:46:08 +0700

    WIP: Fold pada Expr

--- latihantambahan.hs
+++ latihantambahan.hs
@@ -1,5 +1,7 @@
 data Expr = C Integer 
+          | Empty
           | Add Expr Expr 
+          | Branch Expr Expr Expr
           deriving (Eq,Show)
 
 --Latihan satu
@@ -9,13 +11,13 @@ mapList f (x:xs) = f x : mapList f xs
 
 -- > mapList (\x -> Add x (C 1)) [(C 1), (C 2), (C 3)]
 
+-- foldTree :: (Expr a b c) => (a -> b -> c) -> (Expr Branch a b c) -> a
+
 -- data Tree a = Leaf a | Branch (Tree a) (Tree a)
 
 -- foldTree :: (a -> b -> b) -> b -> Tree a -> b
 -- foldTree f init (Leaf x)  = f x init
 -- foldTree f init (Branch x t1 t2) = foldTree f (f x (foldTree f init t2)) t1
 -- foldTree f init (Branch x t1 t2) = foldTree f (f x (foldTree f init t2)) t1
-
-
 
 -- Latihan dua
