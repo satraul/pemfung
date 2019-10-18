3460fc93624feae0ca7bc8b14b3f9e5b37932713
Satryaji Aulia <satraul@macports.org>
2019-10-19 04:26:23 +0700

    Quicksort

--- quiz.hs
+++ quiz.hs
@@ -1,3 +1,8 @@
 maxTiga :: (Integral a) => a -> a -> a -> a
 maxTiga x y z = x `max` y `max` z
 
+quicksort [] = []
+quicksort (x:xs) = quicksort lesser ++ [x] ++ quicksort larger
+    where
+        lesser = [i | i <- xs, i<=x]
+        larger = [j | j <- xs, j>x]
