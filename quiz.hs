4a52900c88b3f89e6c99f06ff9946ccfdc899036
Satryaji Aulia <satraul@macports.org>
2019-10-19 04:35:14 +0700

    Sieve of Eratos

--- quiz.hs
+++ quiz.hs
@@ -6,3 +6,16 @@ quicksort (x:xs) = quicksort lesser ++ [x] ++ quicksort larger
     where
         lesser = [i | i <- xs, i<=x]
         larger = [j | j <- xs, j>x]
+
+jumlahList (xs) = foldl (+) 0 xs
+
+minus (x:xs) (y:ys) = case (compare x y) of 
+           LT -> x : minus  xs  (y:ys)
+           EQ ->     minus  xs     ys 
+           GT ->     minus (x:xs)  ys
+minus  xs     _     = xs
+
+primesTo m = eratos [2..m]
+    where
+    eratos []     = []
+    eratos (p:xs) = p : eratos (xs `minus` [p, p+p..])
