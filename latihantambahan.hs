8a18f9c1b4ea51f318020ed0c448546b029ad059
Satryaji Aulia <satraul@macports.org>
2019-10-19 05:51:49 +0700

    Imperative Robot Language

--- latihantambahan.hs
+++ latihantambahan.hs
@@ -28,3 +28,16 @@ evaluate' (e1 :/ e2) = evaluate' e1 / evaluate' e2
 -- foldTree f init (Branch x t1 t2) = foldTree f (f x (foldTree f init t2)) t1
 
 -- Latihan dua
+-- Untuk memambahkan kepada data dengan cara "extend" sulit, karena Haskell
+-- bukan bahasa pemograman OOP.
+-- Dengan demikian, lebih baik jika data RobotState kami define ulang dengan:
+-- [...]
+-- , energy :: Int
+-- }
+-- Kemudian, untuk setiap fungsi moveN, dll, harus dihandle kasus dimana
+-- RobotState memiliki energi.
+
+-- mapM_ merupakan fugsi map khusus monad yang tidak mengembalikan
+-- sebuah list, melainkan mengembalikan list kosong. mapM_ digunakan
+-- untuk side effect dari sebuah mapping, bukan hasil map benerannya.
+-- Misal untuk mem print sebuah list.
