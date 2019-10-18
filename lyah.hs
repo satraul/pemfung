d52cfc909910151ace817a3f669367be44546ba0
Satryaji Aulia <satraul@macports.org>
2019-10-18 22:16:52 +0700

    List comprehension, tuples, types

--- lyah.hs
+++ lyah.hs
@@ -4,3 +4,26 @@
 
 ra = [1..20]
 lols = take 12 (cycle "LOL")
+
+--List comprehension
+
+fooBars xs = [ if x < 10 then "FOO" else "BAR" | x <- xs, odd x]
+
+--Tuples
+
+tuple' = fst ("Keluar", False)
+tuple'' = snd (False, "Keluar")
+zip' = zip [1,2,3,4,5] [5,5,5,5,5]
+zip'' = zip [1..] ["one", "two", "three", "four", "five"]
+
+--Types
+--semua operator di haskell (==, ++, +, -) merupakan fungsi
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
