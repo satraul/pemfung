d6e0c9ab3b633c4ff0c1798b4e6ab376d7bb17c1
Satryaji Aulia <satraul@macports.org>
2019-10-18 23:28:56 +0700

    Fix Expr map

--- latihantambahan.hs
+++ latihantambahan.hs
@@ -1,13 +1,21 @@
+data Expr = C Integer 
+          | Add Expr Expr 
+          deriving (Eq,Show)
+
 --Latihan satu
-mapList :: (a -> b) -> [a] -> [b]  
+mapList :: (Expr -> Expr) -> [Expr] -> [Expr]
 mapList _ [] = []  
-mapList f (x:xs) = f x : map' f xs  
+mapList f (x:xs) = f x : mapList f xs
 
-data Tree a = Leaf a | Branch (Tree a) (Tree a)
+-- > mapList (\x -> Add x (C 1)) [(C 1), (C 2), (C 3)]
 
-foldTree :: (a -> b -> b) -> b -> Tree a -> b
-foldTree f init (Leaf x)         = f x init
-foldTree f init (Branch x t1 t2) = foldTree f (f x (foldTree f init t2)) t1
-foldTree f init (Branch x t1 t2) = foldTree f (f x (foldTree f init t2)) t1
+-- data Tree a = Leaf a | Branch (Tree a) (Tree a)
+
+-- foldTree :: (a -> b -> b) -> b -> Tree a -> b
+-- foldTree f init (Leaf x)  = f x init
+-- foldTree f init (Branch x t1 t2) = foldTree f (f x (foldTree f init t2)) t1
+-- foldTree f init (Branch x t1 t2) = foldTree f (f x (foldTree f init t2)) t1
+
+
 
 -- Latihan dua
